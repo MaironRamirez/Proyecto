81faa1760bceab7e472993dcdba0a3cdfbeeb804
MaironRamirez <mairamirez@uniboyaca.edu.co>
2022-05-27 21:35:05 -0500

    Animaciones

---
+++ Funciones/Logica.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Ingeniero Multimedia","Fotografo","Diseñador Grafico","Animador","Modelador 3D","Diseñador Web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
