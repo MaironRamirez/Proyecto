db4fadc88741f6548059c379fc1e2ec020d15355
MaironRamirez <mairamirez@uniboyaca.edu.co>
2022-06-21 22:19:58 -0500

    Actualización Datos

--- Funciones/Logica.js
+++ Funciones/Logica.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","Ingeniero Multimedia","Fotografo","Diseñador Grafico","Animador","Modelador 3D","Diseñador Web"],
+    strings:["","Ingeniero Multimedia","Fotógrafo","Diseñador Gráfico","Animador","Modelador 3D","Diseñador Web"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
